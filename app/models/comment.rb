836b15d86ce9e3580b6bd5ab33f864f6058a2818
TheMickeyMike <macnow@st.amu.edu.pl>
2016-01-19 18:28:11 +0100

    Fixed comments serializator

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
 
-    belongs_to :user
-    belongs_to :statuses
+    belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+    belongs_to :statuses, class_name: 'Status', foreign_key: 'post_id'
     attr_accessible :comment, :post_id, :author_id, :user
 
  	  validates :comment, presence: true,
