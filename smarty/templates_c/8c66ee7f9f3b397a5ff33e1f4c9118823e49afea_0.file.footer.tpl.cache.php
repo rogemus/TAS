4aac12a7137a3a9ff6f7bf016fddb5acfe6827df
Kacper Rogowski <rogemus@gmail.com>
2016-01-24 15:54:16 +0100

    Ciagla walka

---
+++ smarty/templates_c/8c66ee7f9f3b397a5ff33e1f4c9118823e49afea_0.file.footer.tpl.cache.php
@@ -0,0 +1,30 @@
+<?php
+/* Smarty version 3.1.29, created on 2016-01-24 15:49:38
+  from "C:\xampp\htdocs\TAS-Client-PAwel\smarty\templates\module\footer.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_56a4e482544261_73023361',
+  'file_dependency' => 
+  array (
+    '8c66ee7f9f3b397a5ff33e1f4c9118823e49afea' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\TAS-Client-PAwel\\smarty\\templates\\module\\footer.tpl',
+      1 => 1453643691,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_56a4e482544261_73023361 ($_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '81156a4e482542c87_47013109';
+?>
+<footer>
+</footer>
+
+</body>
+</html><?php }
+}
