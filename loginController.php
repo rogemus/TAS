9293255c6c9b1cc2e4e517d0f9d30ed2131e0ba3
Kacper Rogowski <rogemus@gmail.com>
2016-01-27 15:59:01 +0100

    Naprawienie przekierowania po zalogowniu

--- loginController.php
+++ loginController.php
@@ -5,19 +5,13 @@
  * Date: 26.01.2016
  * Time: 22:24
  */
-require_once 'vendor/autoload.php';
-require 'Smarty.php';
-require_once 'guzzle.php';
+require 'vendor/autoload.php';
+require 'guzzle.php';
 define('APP_ID', 'd2132e6db96d110b3eb68a368211933500efbd819abcf4ff652e63abaa335648');
 define('SECRET', 'd39ed914f8dad145833d51ee86492a182519c58769c1d95e17a4c249d0261812');
 
 
 session_start();
-if (isset($_SESSION['token'])) {
-    $smarty->display('index.tpl');
-} else {
-    $smarty->display('login.tpl');
-}
 $request = $client->request('POST', '/oauth/token', array(
     'json' => ['client_id' => APP_ID,
         'client_secret' => SECRET,
@@ -33,7 +27,7 @@ $request = $client->request('POST', '/oauth/token', array(
 $token = json_decode($request->getBody());
 ( isset($token->access_token) ) ? $_SESSION['token'] = $token->access_token : die();
 
-header('Location: ../index.php');
+header("Location: index.php");
 
 
 
